c7176db0333b0cabd0a25c40356136f8fb53f693
Van Taymany <vtaymany@gmail.com>
2020-12-10 22:43:05 -0800

    removes installed node packages

--- index.js
+++ index.js
@@ -40,33 +40,6 @@ inquirer
       message: 'What license does this application use?',
       choices: ['MIT License', 'None'],
     },
-    return `
-# ${data.Title}
-## Table of Contents
-* [Description](#description)
-* [Installation](#installation)
-* [Usage](#usage)
-* [License](#license)
-* [Contributors](#contributors)
-* [Test](#test)
-* [Questions](#questions)
-## Description
-${data.Description}
-## Installation 
-${data.Installation}
-## Usage 
-${data.Usage}
-## License
-${data.License}
-## Contributors
-${data.Contributors}
-## Test
-${data.Test}
-## Questions
-Contact me:
-Github:[${data.Username}](https://github.com/DiLiubarets)
-Email:[${data.Email}](https://github.com/DiLiubarets)
-`;
   ])
   .then((response) =>
     fs.writeFile('READMEE.md', data, (err) =>
